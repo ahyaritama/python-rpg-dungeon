13faf5f4bab5a6f89222821a684fc756766c26ed
cynienaputri <cahyani.adina7268@student.unri.ac.id>
2026-05-14 20:52:19 +0700

    add structure

---
+++ structure/monster.py
@@ -0,0 +1,6 @@
+class Monster:
+    def __init__(self, name, damage, defence, health):
+        self.name = name
+        self.damage = damage
+        self.defence = defence
+        self.health = health
---
+++ structure/player.py
@@ -0,0 +1,6 @@
+class Player:
+    def __init__(self, name, damage, defence, health):
+        self.name = name
+        self.damage = damage
+        self.defence = defence
+        self.health = health
