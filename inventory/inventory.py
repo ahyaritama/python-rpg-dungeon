092f67307f023b544039b920630bb7aa74c27d87
raziqmuflih07 <raziqmuflih30@gmail.com>
2026-05-21 11:52:17 +0700

    update inventory

--- inventory/inventory.py
+++ inventory/inventory.py
@@ -1,4 +1,4 @@
-class Item:
+class InventoryItem:
     def __init__(self, data):
         self.data = data
         self.prev = None
@@ -7,3 +7,13 @@ class Item:
 class Inventory:
     def __init__(self):
         self.head = None
+
+    def search_by_name(self, name):
+        current = self.head
+
+        while current is not None:
+            if current.data == name:
+                return True
+            current = current.next
+
+        return False
