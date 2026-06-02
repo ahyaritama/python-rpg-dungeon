b87e051846f0d829658044c9c175db98cfd2a757
Riyanda Ahyaritama <ahyaritamariyanda5@gmail.com>
2026-06-03 00:53:43 +0700

    util: Add player skills getter setter

--- game/util/player/skills.py
+++ game/util/player/skills.py
@@ -1,5 +1,12 @@
-def get_player_skills(username: str):
-    pass
+def get_player_skills(username: str) -> set[str]:
+    skill_set: set[str] = set()
+    with open(f"game/storage/skills/{username}", "r") as f:
+        for line in f:
+            skill_set.add(str(line).strip())
+    
+    return skill_set
 
-def set_player_skills():
-    pass
+def set_player_skills(username: str, skill_set: set[str]):
+    with open(f"game/storage/skills/{username}", "w") as f:
+        for skill in skill_set:
+            f.write(skill + "\n")
