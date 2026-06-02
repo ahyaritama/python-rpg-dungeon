39b7cee52148a736791ca0f5ef760b973739534b
Riyanda Ahyaritama <ahyaritamariyanda5@gmail.com>
2026-06-02 13:48:03 +0700

    struct: Update import path

--- game/struct/player.py
+++ game/struct/player.py
@@ -1,6 +1,10 @@
 from .character import Character
 
-from ..inventory import Inventory
+from ..inventory import (
+    Equipment,
+    Inventory,
+    Item
+)
 
 class Player(Character):
     def __init__(
@@ -8,13 +12,18 @@ class Player(Character):
         name: str,
         stats: dict[str, int],
         skills,
-        bag: Inventory
+        bag: Inventory,
+        equipment: Equipment
     ):
         self.money, *stats = stats.values()
         self.skills = skills
         self.bag = bag
+        self.equipment = equipment
 
         super().__init__(name, *stats)
     
     def learn_skill(self):
+        pass
+
+    def equip(self, item: Item):
         pass
