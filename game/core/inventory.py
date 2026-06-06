94b3d791a547b1cf7c196b4ced3df3241bfc5c27
Riyanda Ahyaritama <ahyaritamariyanda5@gmail.com>
2026-06-06 14:12:34 +0700

    core: Add search and sort feature

--- game/core/inventory.py
+++ game/core/inventory.py
@@ -1,4 +1,4 @@
-from ..inventory import Inventory, InventoryItem
+from ..inventory import InventoryItem, Item
 from ..struct import Player
 from ..util import (
     clear_screen,
@@ -30,6 +30,8 @@ def show_inventory(player: Player):
                 _show_all(player)
             case "2":
                 _browse(player, player.bag.head)
+            case "3":
+                _search(player)
             case _:
                 break
     pass
@@ -108,6 +110,36 @@ def _browse(player: Player, node: InventoryItem, ok=False):
         if not ok:
             return False
 
+def _search(player: Player):
+    while True:
+        clear_screen()
+        header = "=" * 12 + " INVENTORY " + "=" * 12
+
+        print(f"{'Name    '}: {player.name}")
+        print(f"{'Balance '}: {player.money}")
+        print(header)
+
+        print("Here you can search")
+        print("items by name or code.")
+
+        print("=" * len(header))
+        print("[1] Search by Code")
+        print("[2] Search by Name")
+        print("[*] Back")
+        
+        choice = input("Your Choice: ")
+        match choice:
+            case "1":
+                print("-" * len(header))
+                choice = input("Input Item's Code: ")
+                _show_item(player, player.bag.search_by_code(choice))
+            case "2":
+                print("-" * len(header))
+                choice = input("Input Item's Name: ")
+                _show_item(player, player.bag.search_by_name(choice))
+            case _:
+                break
+        
 
 def _show_all(player: Player):
     while True:
@@ -127,7 +159,101 @@ def _show_all(player: Player):
 
         print("=" * len(header))
         print("[1] Sort by Code")
-        print("[2] Sort by Effect")
+        print("[2] Sort by Name")
         print("[3] Sort by Price")
-        input("[Back]")
-        break
+        print("[*] Back")
+
+        choice = input("Your Choice: ")
+        match choice:
+            case "1":
+                print("-" * len(header))
+                print("[1] Descending")
+                print("[*] Ascending")
+                choice = input("Your Choice: ")
+                match choice:
+                    case "1":
+                        player.bag.sort_by_code(False)
+                    case _:
+                        player.bag.sort_by_code()
+            case "2":
+                print("" + "-" * len(header))
+                print("[1] Descending")
+                print("[*] Ascending")
+                choice = input("Your Choice: ")
+                match choice:
+                    case "1":
+                        player.bag.sort_by_name(False)
+                    case _:
+                        player.bag.sort_by_name()
+            case "3":
+                print("" + "-" * len(header))
+                print("[1] Descending")
+                print("[*] Ascending")
+                choice = input("Your Choice: ")
+                match choice:
+                    case "1":
+                        player.bag.sort_by_price(False)
+                    case _:
+                        player.bag.sort_by_price()
+            case _:
+                break
+
+def _show_item(player: Player, detail: tuple[Item, int]):
+    while True:
+        clear_screen()
+        header = "=" * 12 + " INVENTORY " + "=" * 12
+
+        print(f"{'Name    '}: {player.name}")
+        print(f"{'Balance '}: {player.money}")
+        print(header)
+
+        if detail[0] is None:
+            print("Item Not Found!")
+            print("=" * len(header))
+            input("[Back]")
+            break
+
+        item = detail[0]
+        qty = detail[1]
+            
+        print(f"{'Name   '}: {item.name}")
+        print(f"{'Code   '}: {item.code}")
+        print(f"{'Effect '}: {str(item.code).split("_", 1)[0]} +{item.effect}")
+        print(f"{'Price  '}: {item.price}")
+        print(f"{'Qty    '}: {qty}")
+
+        print("=" * len(header))
+        print("[1] Use Item")
+        print("[2] Sell Item")
+        print("[*] Back")
+
+        choice = input("Your Choice:")
+        match choice:
+            case "1":
+                ok, msg = player.equip(item.data)
+                set_player_stats(player)
+                print(msg)
+                input("[OK]")
+                if ok is False:
+                    continue
+                
+                item.qty -= 1
+                if item.qty <= 0:
+                    player.bag.delete(item.data)
+                    set_player_items(player.name, player.bag)
+                    break
+                set_player_items(player.name, player.bag)
+            case "2":
+                player.money += item.data.price
+                set_player_stats(player)
+                print(f"Successfully sell {item.data.name}")
+                input("[OK]")
+
+                item.qty -= 1
+                if item.qty <= 0:
+                    player.bag.delete(item.data)
+                    set_player_items(player.name, player.bag)
+                    break
+                set_player_items(player.name, player.bag)
+            case _:
+                break
